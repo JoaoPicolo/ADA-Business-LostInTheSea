b3aa69773a2e98df9b2372f85337f27968f88c36
João Picolo <picolojoaop@gmail.com>
2022-02-03 10:29:25 -0300

    [main] Added obstacle structure

--- keep-swimming/Utils/Obstacles.swift
+++ keep-swimming/Utils/Obstacles.swift
@@ -5,11 +5,25 @@
 //  Created by João Pedro Picolo on 03/02/22.
 //
 
+import SpriteKit
+
 struct Obstacle {
     var position: ObjectPosition
     var imageSequence: [String]
+    var range: Int = 0
+    var damage: CGFloat
+    var contactEnabled: Bool = true
 }
 
 struct Obstacles {
-    static let seaweed1 = Obstacle()
+    static let seaweed1 = Obstacle(position: .bottom, imageSequence: ["s1", "s2", "s3", "s1"], damage: 5)
+    static let seaweed2 = Obstacle(position: .bottom, imageSequence: [], damage: 5)
+    static let seaweed3 = Obstacle(position: .top, imageSequence: [], damage: 5)
+    
+    static let fishes = Obstacle(position: .top, imageSequence: [], damage: 10)
+    static let starsea = Obstacle(position: .top, imageSequence: [], damage: 5)
+    static let whale = Obstacle(position: .top, imageSequence: [], damage: 30)
+    static let turtle = Obstacle(position: .top, imageSequence: [], damage: 5)
+    static let jellyfish = Obstacle(position: .top, imageSequence: [], damage: 15)
 }
+
